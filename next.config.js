69304396eef703380ca786b6fd2d3c75b01ab3f2
Carlos Rivas Pimentel <106785628+carp22@users.noreply.github.com>
2023-12-19 12:18:18 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export",
-    basePath: "/carp22.github.io",
-    images: {
-        unoptimized: true,
-        remotePatterns: [{
-        protocol: "https",
-        hostname: "drive.google.com",
-        port: "",
-        pathname: "/uc"
-        }],
-    },
+    // output:"export",
+    // basePath: "/carp22.github.io",
+    // images: {
+    //     unoptimized: true,
+    //     remotePatterns: [{
+    //     protocol: "https",
+    //     hostname: "drive.google.com",
+    //     port: "",
+    //     pathname: "/uc"
+    //     }],
+    // },
 }
 
 module.exports = nextConfig
