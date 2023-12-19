e67154bd3c5f0d16d726fca7bf151739ae698001
Carlos Rivas Pimentel <106785628+carp22@users.noreply.github.com>
2023-12-19 12:16:28 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,10 +5,10 @@ const nextConfig = {
     images: {
         unoptimized: true,
         remotePatterns: [{
-            protocol: "https",
-            hostname: "drive.google.com",
-            port: "",
-            pathname: "/uc"
+        protocol: "https",
+        hostname: "drive.google.com",
+        port: "",
+        pathname: "/uc"
         }],
     },
 }
